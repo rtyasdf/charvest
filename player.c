91816d484010f334ced3aad6db8f96cbe2f8aad8
kirill <donthaveone>
2022-06-23 15:32:07 +0300

    agent structure and its initialization

---
+++ player.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+
+typedef struct{
+  char id;
+  int pos_x;
+  int pos_y;
+  char orientation;
+  int reward;
+} HarvestAgent;
+
+
+HarvestAgent* create_agent(char id, int pos_x, int pos_y, char orientation){
+  HarvestAgent* agent = (HarvestAgent* ) malloc(sizeof(HarvestAgent));
+
+  agent -> id = id;
+  agent -> pos_x = pos_x;
+  agent -> pos_y = pos_y;
+  agent -> orientation = orientation;
+  agent -> reward = 0;
+  
+  return agent;
+}
+
+
+char consume(HarvestAgent* agent, char c){
+  if (c == 'A')
+    agent -> reward++;
+    return ' ';
+  return c;
+}
